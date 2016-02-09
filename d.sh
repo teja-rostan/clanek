d83409eb364fec91679b37bc03dad5b8869d5c74
Tomaz Curk <tomaz.curk@fri.uni-lj.si>
2016-02-09 10:43:47 +0100

    Add aux files to ignore list, generate diffs

---
+++ d.sh
@@ -0,0 +1,24 @@
+n="clanek.tex"
+o="clanek_20160209.tex"
+
+d="diff_${n%.tex}"
+nn=${n%.tex}
+
+
+pdflatex $nn
+pdflatex $nn
+bibtex $nn
+bibtex $nn
+pdflatex $nn
+pdflatex $nn
+open $nn.pdf
+
+latexdiff $o $n > $d.tex
+pdflatex $d
+pdflatex $d
+bibtex $d
+bibtex $d
+pdflatex $d
+pdflatex $d
+#open $d.pdf
+
